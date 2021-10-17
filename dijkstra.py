d3c84d7cd23a1ff04191a35c838ff81fe65726b2
duyl97 <dl97.app@gmail.com>
2021-10-17 20:37:56 +0700

    first commit

---
+++ dijkstra.py
@@ -0,0 +1,7 @@
+graph = {}
+graph["you"] = ["alice", "bob", "claire"]
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+print(graph["start"].keys())
+print(graph["start"]["a"])
